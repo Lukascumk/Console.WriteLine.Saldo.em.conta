9bf8f5566bcd8dee80e827de262fd5709e357391
Lukas Mota <lukas_motta@outlook.com>
2024-03-28 17:46:39 +0000

    Refatorei ContaPF e seus parametros

--- ContaPF.cs
+++ ContaPF.cs
@@ -2,15 +2,33 @@ namespace ContasPF
 {
     public class ContaPF
     {
-        public string nome;
-        public string cpf;
-        public float saldo;
+        private string nome;
+        private string cpf;
+        private float saldo;
 
         public ContaPF(string nome, string cpf, float saldo)
         {
             this.nome = nome;
             this.cpf = cpf;
             this.saldo = saldo;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string CPF
+        {
+            get { return cpf; }
+            set { cpf = value; }
+        }
+
+        public float Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
         }
     }
 }
--- main.cs
+++ main.cs
@@ -1,24 +1,28 @@
 using System;
+using ContasPF;
+using ContasPJ;
 
 class Program
 {
     public static void Main()
     {
+
       
-        ContasPF.ContaPF contaPF1 = new ContasPF.ContaPF
+      
+      ContasPF.ContaPF contaPF1 = new ContaPF
       (nome: "João", cpf: "123.456.789-00", saldo: 1000.0f);
         
       ContasPJ.ContaPJ contaPJ1 = new ContasPJ.ContaPJ
       (nome: "Maria", cnpj: "98765432100", saldo: 5000.0f);
 
       
-        if (contaPF1.saldo <= 0)
+        if (contaPF1.Saldo <= 0)
         {
-            Console.WriteLine("Saldo insuficiente: " + contaPF1.saldo);
+            Console.WriteLine("Saldo insuficiente: " + contaPF1.Saldo);
         }
         else
         {
-            Console.WriteLine("Saldo: " + contaPF1.saldo);
+            Console.WriteLine("Saldo: " + contaPF1.Saldo);
         }
 
         if (contaPJ1.saldo >= 0)
