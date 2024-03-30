f46d1e3621ffae6aad777bdc0728ec1b133b36cd
Lukas Mota <lukas_motta@outlook.com>
2024-03-30 18:57:49 -0300

    refatoração

--- ContaPF.cs
+++ ContaPF.cs
@@ -3,31 +3,14 @@ namespace ContasPF
     public class ContaPF
     {
         public string nome;
-        private string cpf;
-        private float saldo;
+        public string cpf;
+        public float saldo;
 
         public ContaPF(string nome, string cpf, float saldo)
         {
             this.nome = nome;
             this.cpf = cpf;
             this.saldo = saldo;
-        }
-        public string Nome
-        {
-            get { return nome; }
-            set { nome = value; }
-        }
-
-        public string CPF
-        {
-            get { return cpf; }
-            set { cpf = value; }
-        }
-
-        public float Saldo
-        {
-            get { return saldo; }
-            set { saldo = value; }
         }
     }
 }
