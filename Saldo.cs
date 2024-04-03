96aaf1e650b12a007fe2d55d7a85cdafe2656899
Lukas Mota <lukas_motta@outlook.com>
2024-04-03 17:43:03 +0000

    Saldo.conta

--- Saldo.cs
+++ Saldo.cs
@@ -5,26 +5,31 @@ namespace Transacao
         public float saldoF;
         public int saldoI;
         public decimal saldoD;
+      
+      public SaldoConta(float saldoF, int saldoI, decimal saldoD)
+      {
+          this.saldoF = saldoF;
+          this.saldoI = saldoI;
+          this.saldoD = saldoD;
+      }
 
+      
         public class TransacaoConta
         {
             public string debito;
             public string credito;
             public string pix;
 
-            public SaldoConta(float saldoF, int saldoI, decimal saldoD)
-            {
-                this.saldoF = saldoF;
-                this.saldoI = saldoI;
-                this.saldoD = saldoD;
-            }
-
+            
             public TransacaoConta(string debito, string credito, string pix)
             {
                 this.debito = debito;
                 this.credito = credito;
                 this.pix = pix;
             }
+  
+         
+           
         }
     }
 }
