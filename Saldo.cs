2dd6c77eb3e22af5b82821254ed9afa436afdb80
Lukas Mota <lukas_motta@outlook.com>
2024-04-03 20:18:58 +0000

    Adc Class transacao

--- Saldo.cs
+++ Saldo.cs
@@ -5,31 +5,37 @@ namespace Transacao
         public float saldoF;
         public int saldoI;
         public decimal saldoD;
-      
-      public SaldoConta(float saldoF, int saldoI, decimal saldoD)
-      {
-          this.saldoF = saldoF;
-          this.saldoI = saldoI;
-          this.saldoD = saldoD;
-      }
 
-      
-        public class TransacaoConta
+        public SaldoConta(float saldoF, int saldoI, decimal saldoD)
         {
-            public string debito;
-            public string credito;
-            public string pix;
-
-            
-            public TransacaoConta(string debito, string credito, string pix)
-            {
-                this.debito = debito;
-                this.credito = credito;
-                this.pix = pix;
-            }
-  
-         
-           
+            this.saldoF = saldoF;
+            this.saldoI = saldoI;
+            this.saldoD = saldoD;
         }
+
+        public void AdicionarSaldo(float valor)
+        {
+            saldoF += valor;
+        }
+
+        public void SubtrairSaldo(float valor)
+        {
+            saldoF -= valor;
+        }
+      
     }
+  public class TransacaoConta
+  {
+      public string debito;
+      public string credito;
+      public string pix;
+
+      public TransacaoConta(string debito, string credito, string pix)
+      {
+          this.debito = debito;
+          this.credito = credito;
+          this.pix = pix;
+      }
+  }
+  
 }
