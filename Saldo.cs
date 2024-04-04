bc4014a678830fce6cd93d9a53f2af13963fd975
Lukas Mota (Vs) <lukas_motta@outlook.com>
2024-04-04 00:50:14 -0300

    Refatoração class transacao

--- Saldo.cs
+++ Saldo.cs
@@ -5,31 +5,58 @@ namespace Transacao
         public float saldoF;
         public int saldoI;
         public decimal saldoD;
-      
-      public SaldoConta(float saldoF, int saldoI, decimal saldoD)
-      {
-          this.saldoF = saldoF;
-          this.saldoI = saldoI;
-          this.saldoD = saldoD;
-      }
+        public TransacaoConta TransacaoConta;
 
-      
+        public SaldoConta(float saldoF, int saldoI, decimal saldoD)
+        {
+            this.saldoF = saldoF;
+            this.saldoI = saldoI;
+            this.saldoD = saldoD;
+            this.TransacaoConta = new TransacaoConta();
+        }
+
+
         public class TransacaoConta
         {
             public string debito;
             public string credito;
             public string pix;
 
-            
+            public void RealizarDebito(float valor)
+            {
+                this.saldof -= valor;
+                this.saldoI -= valor;
+                this.saldoD -= valor;
+            }
+
+
+            public void RealizarCredito(float valor)
+            {
+                this.saldof -= valor;
+                this.saldoI -= valor;
+                this.saldoD -= valor;
+            }
+
+            public void RealizarPix(float valor)
+            {
+                if (this.saldo > 0)
+                {
+                    saldo += valor;
+                    Console.WriteLine($"Transação de {valor:c} Realizada. Novo saldo: {saldo:c}");
+                }
+                else if (valor < 0)
+                {
+                    saldo -= Math.Abs(valor);
+                    Console.WriteLine($"Transação de {valor:c} realizada. Novo saldo:{saldo:c}");
+                }
+                else
+                {
+                    Console.WriteLine("Trasação negada.");
+                }
+            }
             public TransacaoConta(string debito, string credito, string pix)
             {
                 this.debito = debito;
                 this.credito = credito;
                 this.pix = pix;
             }
-  
-         
-           
-        }
-    }
-}
