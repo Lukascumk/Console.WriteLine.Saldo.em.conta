af2553ec09697b6cffdca1e6ad0b01893129883a
Lukas Mota <lukas_motta@outlook.com>
2024-04-04 16:49:24 +0000

    reestruturacao Classes

--- Transacao.cs
+++ Transacao.cs
@@ -1,42 +1,62 @@
 namespace Transacao
 {
-    public class TransacaoConta
-    {
-        public string debito;
-        public string credito;
-        public string pix;
-
-        public TransacaoConta(string debito, string credito, string pix)
-        {
-            this.debito = debito;
-            this.credito = credito;
-            this.pix = pix;
-        }
     public class SaldoConta
     {
         public float saldoF;
         public int saldoI;
         public decimal saldoD;
+        public TransacaoConta TransacaoConta;
 
         public SaldoConta(float saldoF, int saldoI, decimal saldoD)
         {
             this.saldoF = saldoF;
             this.saldoI = saldoI;
             this.saldoD = saldoD;
+            this.TransacaoConta = new TransacaoConta();
         }
 
-        public void AdicionarSaldo(float valor)
-        {
-            saldoF += valor;
-        }
 
-        public void SubtrairSaldo(float valor)
+        public class TransacaoConta
         {
-            saldoF -= valor;
-        }
-      
-    }
-  
-  }
-  
-}
+            public string debito;
+            public string credito;
+            public string pix;
+
+            public void RealizarDebito(float valor)
+            {
+                this.saldof -= valor;
+                this.saldoI -= valor;
+                this.saldoD -= valor;
+            }
+
+
+            public void RealizarCredito(float valor)
+            {
+                this.saldof -= valor;
+                this.saldoI -= valor;
+                this.saldoD -= valor;
+            }
+
+            public void RealizarPix(float valor)
+            {
+                if (this.saldo > 0)
+                {
+                    saldo += valor;
+                    Console.WriteLine($"Transação de {valor:c} Realizada. Novo saldo: {saldo:c}");
+                }
+                else if (valor < 0)
+                {
+                    saldo -= Math.Abs(valor);
+                    Console.WriteLine($"Transação de {valor:c} realizada. Novo saldo:{saldo:c}");
+                }
+                else
+                {
+                    Console.WriteLine("Trasação negada.");
+                }
+            }
+            public TransacaoConta(string debito, string credito, string pix)
+            {
+                this.debito = debito;
+                this.credito = credito;
+                this.pix = pix;
+            }
