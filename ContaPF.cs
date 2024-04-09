deea03671b4b84d5702a505a33398b570b283fb5
Lukas Mota <lukas_motta@outlook.com>
2024-04-09 19:43:21 +0000

    Verificação de Senha

--- ContaPF.cs
+++ ContaPF.cs
@@ -3,12 +3,14 @@ namespace ContasPF
     public class ContaPF
     {
         private string nome;
+        private string senha;
         private string cpf;
         private float saldo;
-    public ContaPF(string nome, string cpf, float saldo)
+    public ContaPF(string nome, string cpf, string senha, float saldo)
         {
             this.nome = nome;
             this.cpf = cpf;
+            this.senha = senha;
             this.saldo = saldo;
         }
         public string Nome
@@ -16,10 +18,16 @@ namespace ContasPF
             get { return nome; }
             set { nome = value; }
         }
+        
         public string CPF
         {
             get { return cpf; }
             set { cpf = value; }
+        }
+        public string Senha
+        {
+            get{return senha;}
+            set{senha = value;}
         }
         public float Saldo
         {
--- ContaPJ.cs
+++ ContaPJ.cs
@@ -4,29 +4,47 @@ namespace ContasPJ
     {
         private string nomeEmpresa;
         private string cnpj;
-        private float saldoEmpresa;
-      
-      public ContaPJ  
-      (string nomeEmpresa, string cnpj, float saldoEmpresa)
+        private string senha;
+
+        public ContaPJ(string nomeEmpresa, string cnpj, string senha)
         {
             this.nomeEmpresa = nomeEmpresa;
             this.cnpj = cnpj;
-            this.saldoEmpresa = saldoEmpresa;
+            this.senha = senha;
         }
-              public string NomeEmpresa
-              {
-                  get { return nomeEmpresa; }
-                  set { nomeEmpresa = value; }
-              }
-              public string CNPJ
-              {
-                  get { return cnpj; }
-                  set { cnpj = value; }
-              }
-              public float SaldoEmpresa
-              {
-                  get { return saldoEmpresa; }
-                  set { saldoEmpresa = value; }
-              }
+
+        public string NomeEmpresa
+        {
+            get { return nomeEmpresa; }
+            set { nomeEmpresa = value; }
+        }
+
+        public string CNPJ
+        {
+            get { return cnpj; }
+            set { cnpj = value; }
+        }
+
+        public string Senha
+        {
+            get { return senha; }
+            set
+            {
+                if (value.Length != 6)
+                {
+                    throw new ArgumentException("A Senha deve conter apenas 6 dígitos");
+                }
+                if (!ValorNumerico(value))
+                {
+                    throw new ArgumentException("A Senha deve conter apenas números");
+                }
+                senha = value;
+            }
+        }
+
+        private bool ValorNumerico(string valor)
+        {
+            return valor.All(char.IsDigit);
+        }
     }
 }
